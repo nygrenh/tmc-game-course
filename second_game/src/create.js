62d45bc3fac022aa58c08ef5e63e20b69d8b2e74
Henrik Nygren <nygrenh@gmail.com>
2016-01-12 12:17:46 +0200

    Add test locks

--- second_game/src/create.js
+++ second_game/src/create.js
@@ -1,10 +1,10 @@
 var player;
 var platforms;
 var cursors;
-
+// LOCK
 var score = 0;
 var scoreText;
-
+// END LOCK
 function create() {
 
     player = game.add.sprite(100, 587, 'player');
@@ -14,18 +14,18 @@ function create() {
     player.body.collideWorldBounds = true;
 
     platforms = game.add.physicsGroup();
-
+    // LOCK
     setInterval(() => {
         platforms.create(Math.floor(Math.random() * window.innerWidth), 0, 'platform');
     },  500);
 
     platforms.setAll('checkWorldBounds', true);
     platforms.setAll('outOfBoundsKill', true);
-
+    // END LOCK
     game.physics.arcade.enable(platforms);
 
     cursors = game.input.keyboard.createCursorKeys();
-
+    // LOCK
     scoreText = game.add.text(16, 16, 'Pisteet: 0', { fontSize: '32px', fill: '#ff0000' });
-
+    // END LOCK
 }
