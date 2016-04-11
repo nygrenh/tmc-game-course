ff63d461ffc38ef53aae6c4985b894c28661cf86
Henrik Nygren <nygrenh@gmail.com>
2016-04-11 16:12:09 +0300

    Update t4

--- t4/src/preload.js
+++ t4/src/preload.js
@@ -3,9 +3,8 @@ function preload() {
     game.load.baseURL = 'https://henriknygren.fi/';
     game.load.crossOrigin = 'anonymous';
 
-    game.stage.backgroundColor = '#000';
+    game.stage.backgroundColor = '#2980b9';
 
-    game.load.image('player', 'assets/player.png');
-    game.load.image('platform', 'assets/platform.png');
-
+    game.load.image('player', 'assets/green.png');
+    game.load.image('platform', 'assets/gem.png');
 }
