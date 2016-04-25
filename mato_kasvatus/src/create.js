7900a0b20d900a41cf46d573fc2c15855657bf06
Henrik Nygren <nygrenh@gmail.com>
2016-04-25 16:10:40 +0300

    Add mato kasvatus

---
+++ mato_kasvatus/src/create.js
@@ -0,0 +1,47 @@
+var mato;
+var omena;
+var suunta;
+var koko;
+var pisteet;
+var pisteetTekstinä;
+
+var ruudunKoko;
+var näppäimet;
+
+function create() {
+    mato = [];
+    omena = {};
+    suunta = 'alas';
+
+    koko = 1;
+
+    pisteet = 0;
+    ruudunKoko = 20;
+
+    näppäimet = game.input.keyboard.createCursorKeys();
+
+    game.stage.backgroundColor = '#8EB367';
+
+    luoMato();
+    uusiOmena();
+
+    pisteetTekstinä = game.add.text(30, 20, 'Score: 0', { font: 'bold 14px sans-serif', fill: '#36381B', align: 'center' });
+
+    this.game.time.slowMotion = 10;
+
+    setInterval(kerranSekunnissa, 1000);
+}
+
+function luoMato() {
+    luoMadonOsa(5, 5);
+}
+
+function luoMadonOsa(x, y) {
+    mato.push(game.add.sprite(x * ruudunKoko, y * ruudunKoko, 'mato'));
+}
+
+function kerranSekunnissa() {
+    // LOCK FROM BEGINNING
+    // Kirjota koodisi tänne
+    // LOCK TO END
+}
