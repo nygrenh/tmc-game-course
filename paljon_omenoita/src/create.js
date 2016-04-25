f9e43815cc92748d76966d5d8e7128568cf5049a
Henrik Nygren <nygrenh@gmail.com>
2016-04-25 16:37:10 +0300

    Add paljon omenoita

---
+++ paljon_omenoita/src/create.js
@@ -0,0 +1,40 @@
+var mato;
+var omena;
+var suunta;
+var koko;
+var pisteet;
+var pisteetTekstinä;
+
+var ruudunKoko;
+var näppäimet;
+
+function create() {
+    mato = [];
+    omena = {};
+
+    suunta = 'alas';
+    koko = 4;
+    pisteet = 0;
+    ruudunKoko = 20;
+
+    näppäimet = game.input.keyboard.createCursorKeys();
+
+    game.stage.backgroundColor = '#8EB367';
+
+    luoMato();
+    // LOCK FROM BEGINNING
+    uusiOmena();
+    // LOCK TO END
+
+    pisteetTekstinä = game.add.text(30, 20, 'Score: 0', { font: 'bold 14px sans-serif', fill: '#36381B', align: 'center' });
+
+    this.game.time.slowMotion = 10;
+}
+
+function luoMato() {
+    luoMadonOsa(35, 25);
+}
+
+function luoMadonOsa(x, y) {
+    mato.push(game.add.sprite(x * ruudunKoko, y * ruudunKoko, 'mato'));
+}
---
+++ paljon_omenoita/src/tormaykset.js
@@ -0,0 +1,22 @@
+function törmääItseensä() {
+    var madonPää = mato[0];
+    for (var i = 1; i < mato.length; i++) {
+        if (madonPää.x === mato[i].x && madonPää.y === mato[i].y) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function törmääSeinään() {
+    var madonPää = mato[0];
+    if (madonPää.x >= this.game.width || madonPää.x < 0 || madonPää.y >= this.game.height || madonPää.y < 0) {
+        return true;
+    }
+    return false;
+}
+
+function törmääOmenaan() {
+    var madonPää = mato[0];
+    return madonPää.x == omena.x && madonPää.y == omena.y;
+}
