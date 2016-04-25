7e97b8e241a97a4cb807b7ccaac9802877ade253
Henrik Nygren <nygrenh@gmail.com>
2016-04-25 15:46:57 +0300

    Update mato seinät

--- mato_seinat/src/tormaykset.js
+++ mato_seinat/src/tormaykset.js
@@ -16,7 +16,7 @@ function törmääSeinään() {
     var mennytOikealtaYli = madonPää.x >= pelinLeveys;
     var mennytVasemmaltaYli = madonPää.x < 0;
     // LOCK FROM BEGINNING
-    var mennytAlhaaltayli = false
+    var mennytAlhaaltayli = false;
     var mennytYlhäältäYli = false;
     // LOCK TO END
     if (mennytOikealtaYli || mennytVasemmaltaYli || mennytAlhaaltayli || mennytYlhäältäYli) {
