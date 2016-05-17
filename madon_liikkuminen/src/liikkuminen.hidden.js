9aad8c650cf8987a5588e418ceab7f345eca6132
Henrik Nygren <nygrenh@gmail.com>
2016-05-17 11:20:44 +0300

    Hide madon liikkuminen

---
+++ madon_liikkuminen/src/liikkuminen.hidden.js
@@ -0,0 +1,20 @@
+function liikutaHäntää() {
+    if (mato.length > koko) {
+        mato.pop().destroy();
+    }
+}
+
+function lueSuunta() {
+    if (näppäimet.right.isDown && suunta !== 'vasemmalle') {
+        suunta = 'oikealle';
+    }
+    if (näppäimet.left.isDown && suunta !== 'oikealle') {
+        suunta = 'vasemmalle';
+    }
+    if (näppäimet.up.isDown && suunta !== 'alas') {
+        suunta = 'ylös';
+    }
+    if (näppäimet.down.isDown && suunta !== 'ylös') {
+        suunta = 'alas';
+    }
+}
