7e9340dd8d95fee6529264449b4bf041a85112f6
Henrik Nygren <nygrenh@gmail.com>
2016-05-23 15:40:21 +0300

    Add lauseen muodostaminen

---
+++ lauseen_muodostaminen/src/yhdistele.js
@@ -0,0 +1,6 @@
+var a = "Tänään ";
+var b = "kävin kaupassa";
+// LOCK FROM BEGINNING
+var yhdistetty = ;
+// LOCK TO END
+console.log(yhdistetty);
---
+++ pitkan_lauseen_muodostaminen/src/yhdistele.js
@@ -0,0 +1,7 @@
+var a = "Sanojen yhdisteleminen ";
+var b = "on helpompaa ";
+var c = "kuin kuvittelin."
+// LOCK FROM BEGINNING
+var yhdistetty = ;
+// LOCK TO END
+console.log(yhdistetty);
