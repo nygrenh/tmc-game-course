b8a4bb94daf4c187b5619bc1517afc5aca755db5
Henrik Nygren <nygrenh@gmail.com>
2016-08-01 16:31:21 +0300

    Simplify t4

--- t4/src/update.hidden.js
+++ t4/src/update.hidden.js
@@ -13,3 +13,11 @@ function update () {
 
     platforms.setAll('body.velocity.y', palkinNopeus)
 }
+
+function muutaSuuntaa() {
+    if (cursors.left.isDown) {
+        player.body.velocity.x = pallonNopeus
+    } else if (cursors.right.isDown) {
+        player.body.velocity.x = -1 * pallonNopeus
+    }
+}
