d69ce12da200156cd84f6c3a1e27a4d9f67a0c7d
Henrik Nygren <nygrenh@gmail.com>
2016-08-02 11:46:27 +0300

    Listojen harjoittelu

--- listojen_harjoittelu/test/pointless_test.js
+++ listojen_harjoittelu/test/pointless_test.js
@@ -1,1 +1,26 @@
-console.log('This test is pointless.')
+describe('Stattiset testit', function() {
+    it('Koodi kääntyy', function() {
+        expect(importingFile('listat.js')).not.to.throw(Error, 'Kirjoitit jotakin, mitä tietokone ei ymmärtänyt.');
+    });
+});
+
+describe('Tulostus', function() {
+    var console;
+
+    beforeEach(function() {
+        console = {};
+        console.log = sinon.spy();
+    });
+
+    it('Tulostusta on kutsuttu listalla', function() {
+        eval(fileContents('listat.js'));
+        var a = console.log.firstCall.args[0];
+        expect(a).to.be.type('Array', 'Muuttuja lista ei ollut lista. Varmista, että muuttujan määrittelyssä on merkit [] sisällön ympärillä.');
+    });
+
+    it('Tulostettu lista on oikea', function() {
+        eval(fileContents('listat.js'));
+        var a = console.log.firstCall.args[0];
+        expect(a).to.deep.equal([1, 2, "listat", "ovat", "hauskoja!"], 'Listan sisältö ei ollut oikea. Varmista että tulostus on täsmälleen samanlainen kun tehtävänannon esimerkki.')
+    });
+});
