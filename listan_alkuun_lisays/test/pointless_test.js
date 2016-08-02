4d7f63bdfd685c2f11afe45f2f91c9646a589e9d
Henrik Nygren <nygrenh@gmail.com>
2016-08-02 11:52:56 +0300

    Add tests for listan alkuun lisäys

--- listan_alkuun_lisays/test/pointless_test.js
+++ listan_alkuun_lisays/test/pointless_test.js
@@ -1,1 +1,20 @@
-console.log('This test is pointless.')
+describe('Stattiset testit', function() {
+    it('Koodi kääntyy', function() {
+        expect(importingFile('listat.js')).not.to.throw(Error, 'Kirjoitit jotakin, mitä tietokone ei ymmärtänyt.');
+    });
+});
+
+describe('Tulostus', function() {
+    var console;
+
+    beforeEach(function() {
+        console = {};
+        console.log = sinon.spy();
+    });
+
+    it('Tulostettu lista on oikea', function() {
+        eval(fileContents('listat.js'));
+        var a = console.log.firstCall.args[0];
+        expect(a).to.deep.equal([2, 3, 4], 'Listan sisältö ei ollut oikea. Varmista, että tulostus on [2, 3, 4].');
+    });
+});
