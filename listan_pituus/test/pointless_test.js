26b496265a91d09dffb4594fd6c7a9cd7f210401
Henrik Nygren <nygrenh@gmail.com>
2016-08-02 12:05:19 +0300

    Add tests for listan pituus

--- listan_pituus/test/pointless_test.js
+++ listan_pituus/test/pointless_test.js
@@ -1,1 +1,23 @@
-console.log('This test is pointless.')
+describe('Stattiset testit', function() {
+    it('Ei tulosteta vastausta suoraan', function() {
+        expect(fileContents('listat.js')).not.to.match(/^[ \t]*console.log[ \t]*([ \t]*[^\n]*\d+[^\n]*[ \t]*)[ \t]*$/m, 'Älä yritä tulostaa vastausta suoraan vaan selvitä listan pituus materiaalisssa esitetyllä tavalla.')
+    });
+    it('Koodi kääntyy', function() {
+        expect(importingFile('listat.js')).not.to.throw(Error, 'Kirjoitit jotakin, mitä tietokone ei ymmärtänyt.');
+    });
+});
+
+describe('Tulostus', function() {
+    var console;
+
+    beforeEach(function() {
+        console = {};
+        console.log = sinon.spy();
+    });
+
+    it('Tulostettu lista on oikea', function() {
+        eval(fileContents('listat.js'));
+        var a = console.log.firstCall.args[0];
+        expect(a).to.equal(497, 'Väitit listassa olevan #{act} alkiota, mutta se ei ole oikea vastaus');
+    });
+});
