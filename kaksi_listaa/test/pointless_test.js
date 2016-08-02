c9029ed9a8b56cc16b5b8dfc21647318d9a6f792
Henrik Nygren <nygrenh@gmail.com>
2016-08-02 12:10:59 +0300

    Add tests for kaksi listaa

--- kaksi_listaa/test/pointless_test.js
+++ kaksi_listaa/test/pointless_test.js
@@ -1,1 +1,23 @@
-console.log('This test is pointless.')
+describe('Stattiset testit', function() {
+    it('Ei tulosteta vastausta suoraan', function() {
+        expect(fileContents('listat.js')).not.to.match(/^[ \t]*console.log[ \t]*([ \t]*[^\n]*\d+[^\n]*[ \t]*)[ \t]*$/m, 'Älä yritä tulostaa vastausta suoraan vaan selvitä vastaus hyöndyntämällä materiaalin menetelmiä ja laskutoimituksia.')
+    });
+    it('Koodi kääntyy', function() {
+        expect(importingFile('listat.js')).not.to.throw(Error, 'Kirjoitit jotakin, mitä tietokone ei ymmärtänyt.');
+    });
+});
+
+describe('Tulostus', function() {
+    var console;
+
+    beforeEach(function() {
+        console = {};
+        console.log = sinon.spy();
+    });
+
+    it('Tulostettu lista on oikea', function() {
+        eval(fileContents('listat.js'));
+        var a = console.log.firstCall.args[0];
+        expect(a).to.equal(1099, 'Väitit, että jalokiviä ostetaan #{act} kappaletta, mutta se ei ole oikea vastaus');
+    });
+});
