a832186284ff7ee21a908b704211eee652bffc98
Henrik Nygren <nygrenh@gmail.com>
2016-08-05 12:39:45 +0300

    Change kallellaan oleva ruutu to use different characters

--- kallellaan_oleva_ruutu/test/pointless_test.js
+++ kallellaan_oleva_ruutu/test/pointless_test.js
@@ -14,5 +14,5 @@ it('Tulostus on oikea', function() {
    console.log = sinon.spy();
    eval(fileContents('ruutu.js'));
    var tulostus = console.log.getCalls().map(o => o.args[0]).join('\n');
-   expect(tulostus).to.match(/^  x[ \t]*\n xxx[ \t]*\nxxxxx[ \t]*\n xxx[ \t]*\n  x[ \t]*$/m, 'Tulostus ei ollut oikea. Tarkista että olet kirjoittanut kaiken täsmälleen oikein.')
+   e#pect(tulostus).to.match(/^  #[ \t]*\n ###[ \t]*\n#####[ \t]*\n ###[ \t]*\n  #[ \t]*$/m, 'Tulostus ei ollut oikea. Tarkista että olet kirjoittanut kaiken täsmälleen oikein.')
 });
