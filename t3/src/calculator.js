043729d6f98aafb10e4363552981bf7716c1b714
Henrik Nygren <nygrenh@gmail.com>
2016-08-05 15:28:17 +0300

    Make project structure more logical

--- t3/src/calculator.js
+++
@@ -1,6 +0,0 @@
-// Korvaa tämä kommentti muuttujan a alustuksella
-// Korvaa tämä kommentti muuttujan b alustuksella
-// Korvaa tämä kommentti muuttujien a ja b summaamisella
-
-// huom. Jos a = 5 ja b = 2, ruudulle pitäisi tulostua 7
-// Jos ruudulle tulostuu 52, kertaa lukujen ja merkkijonojen ero
--- turtle_2/src/preload.js
+++
@@ -1,5 +0,0 @@
-function preload() {
-    game.load.baseURL = 'https://henriknygren.fi/'
-    game.load.crossOrigin = 'anonymous'
-    game.load.image('ball', 'assets/circle.png')
-}
