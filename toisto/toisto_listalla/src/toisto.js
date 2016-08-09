4a27f6d01ec4ddd97f647ea147c69341757544fd
Henrik Nygren <nygrenh@gmail.com>
2016-08-09 16:44:23 +0300

    Add toisto listalla

---
+++ toisto/toisto_listalla/src/toisto.js
@@ -0,0 +1,12 @@
+var lista = ["Tänään", "oli", "kaunis", "päivä"]
+
+var kutsuja = 0;
+function toistettavaFunktio(alkio) {
+  kutsuja = kutsuja + 1
+
+  console.log("--------------")
+  console.log("Funktion kutsu #" + kutsuja)
+  console.log("Tällä kutsulla sain alkion: " + alkio)
+}
+// LOCK FROM BEGINNING
+lista.forEach( )
