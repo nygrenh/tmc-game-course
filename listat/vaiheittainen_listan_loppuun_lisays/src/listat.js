8df9c1b29815dc8b73a30aed4ee7ffeb30ed388e
Henrik Nygren <nygrenh@gmail.com>
2016-08-17 13:58:05 +0300

    Add vaiheittainen listan loppuun lisäys

---
+++ listat/vaiheittainen_listan_loppuun_lisays/src/listat.js
@@ -0,0 +1,23 @@
+var numerot = [4]
+console.log('Listan sisältö alussa: ' + numerot)
+
+// Lisää alla listan loppuun numero 92
+// LOCK FROM BEGINNING
+
+
+// START LOCK
+console.log('Listan sisältö ensimmäisen lisäyksen jälkeen: ' + numerot)
+
+// Lisää alla listan loppuun numero 11
+// END LOCK
+
+
+// START LOCK
+console.log('Listan sisältö toisen lisäyksen jälkeen: ' + numerot)
+
+// Lisää alla listan loppuun merkkijono "Numerot loppui :("
+// END LOCK
+
+
+// LOCK TO END
+console.log('Listan sisältö lopussa: ' + numerot)
