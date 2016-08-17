c88d2bf01d5a4878f0e939ea3f00a274b939a42f
Henrik Nygren <nygrenh@gmail.com>
2016-08-17 15:34:55 +0300

    Add vaiheittainen listasta poistaminen

---
+++ listat/vaihettainen_poisto/src/listat.js
@@ -0,0 +1,23 @@
+var numerot = [23, 1222, 9999, 'listan loppu :(']
+console.log('Listan sisältö alussa: ' + numerot)
+
+// Poista alla listan lopusta alkio
+// LOCK FROM BEGINNING
+
+
+// START LOCK
+console.log('Listan sisältö ensimmäisen poiston jälkeen: ' + numerot)
+
+// Poista alla listan alusta alkio
+// END LOCK
+
+
+// START LOCK
+console.log('Listan sisältö toisen poiston jälkeen: ' + numerot)
+
+// Poista alla listan lopusta alkio
+// END LOCK
+
+
+// LOCK TO END
+console.log('Listan sisältö lopussa: ' + numerot)
