80aee66c3dee7d3698d526eea59378749c4cdfbb
Henrik Nygren <nygrenh@gmail.com>
2016-08-17 14:57:06 +0300

    Add loppuun ja alkuun lisäys

---
+++ listat/loppuun_ja_alkuun_lisays/src/listat.js
@@ -0,0 +1,30 @@
+var numerot = [3]
+console.log('Listan sisältö alussa: ' + numerot)
+
+// Lisää alla listan alkuun numero 2
+// LOCK FROM BEGINNING
+
+
+// START LOCK
+console.log('Listan sisältö ensimmäisen lisäyksen jälkeen: ' + numerot)
+
+// Lisää alla listan loppuun numero 4
+// END LOCK
+
+
+// START LOCK
+console.log('Listan sisältö toisen lisäyksen jälkeen: ' + numerot)
+
+// Lisää alla listan loppuun numero 5
+// END LOCK
+
+
+// START LOCK
+console.log('Listan sisältö kolmannen lisäyksen jälkeen: ' + numerot)
+
+// Lisää alla listan alkuun numero 1
+// END LOCK
+
+
+// LOCK TO END
+console.log('Listan sisältö lopussa: ' + numerot)
