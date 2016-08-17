83583dcdb898a87cf431fdf2d544170ddbb45261
Henrik Nygren <nygrenh@gmail.com>
2016-08-17 14:33:58 +0300

    Add vaiheittainen listan alkuun lisäys

---
+++ listat/vaiheittainen_listan_alkuun_lisays/src/listat.js
@@ -0,0 +1,23 @@
+var numerot = [4]
+console.log('Listan sisältö alussa: ' + numerot)
+
+// Lisää alla listan alkuun numero 11
+// LOCK FROM BEGINNING
+
+
+// START LOCK
+console.log('Listan sisältö ensimmäisen lisäyksen jälkeen: ' + numerot)
+
+// Lisää alla listan alkuun numero 35
+// END LOCK
+
+
+// START LOCK
+console.log('Listan sisältö toisen lisäyksen jälkeen: ' + numerot)
+
+// Lisää alla listan alkuun numero 2222
+// END LOCK
+
+
+// LOCK TO END
+console.log('Listan sisältö lopussa: ' + numerot)
---
+++ listat/vaiheittainen_listan_alkuun_lisays/test/pointless_test.js
@@ -0,0 +1,19 @@
+describe('Stattiset testit', function() {
+    it('Koodi kääntyy', function() {
+        expect(importingFile('listat.js')).not.to.throw(Error, 'Kirjoitit jotakin, mitä tietokone ei ymmärtänyt.');
+    });
+});
+
+describe('Tulostus', function() {
+    var console;
+
+    beforeEach(function() {
+        console = {};
+        console.log = sinon.spy();
+    });
+
+    it('Tulostettu lista on oikea', function() {
+        eval(fileContents('listat.js'));
+        expect(numerot).to.deep.equal([2222, 35, 11, 4], 'Lopullinen lista ei ollut oikea. Varmista, että lisäät listaan täsmälleen oikeat asiat oikeissa kohdissa.')
+    });
+});
