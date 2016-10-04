585de913104e2bd35c736b644813894d3a966209
Henrik Nygren <nygrenh@gmail.com>
2016-10-04 11:44:30 +0300

    Add tests for listan keskiarvo

--- toisto/listan_keskiarvo/test/pointless_test.js
+++ toisto/listan_keskiarvo/test/pointless_test.js
@@ -1,1 +1,19 @@
-console.log('This test is pointless.')
+context('Staattiset testit', function() {
+    it('Koodi kääntyy', function() {
+        expect(importingFile('listat.js')).not.to.throw(Error, 'Kirjoitit jotakin, mitä tietokone ei ymmärtänyt.');
+    });
+});
+
+context('Dynaamiset testit', function() {
+    var console;
+
+    beforeEach(function() {
+        console = {};
+        console.log = sinon.spy();
+    });
+
+    it('Tulostus on oikea', function() {
+        eval(fileContents('listat.js'));
+        expect(Math.abs(keskiarvo - 18.06756756756757) < 0.00001).to.equal(true, 'Listan keskiarvo ei ollut oikea.');
+    });
+});
