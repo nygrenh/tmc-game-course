edb4e60242a59b322c42ed2b9f00f677aa24b37e
Henrik Nygren <nygrenh@gmail.com>
2016-10-04 10:08:09 +0300

    Add tests for piirra muuri

--- piirtaminen/piirra_muuri/test/pointless_test.js
+++ piirtaminen/piirra_muuri/test/pointless_test.js
@@ -1,1 +1,25 @@
-console.log('This test is pointless.')
+context('Piirtokomennot', function() {
+  var palat;
+  var vasemmalle = function() {
+    palat.push('v');
+  };
+  var oikealle = function() {
+    palat.push('o');
+  };
+  var ylos = function() {
+    palat.push('y');
+  };
+  var alas = function() {
+    palat.push('a');
+  };
+
+  beforeEach(function() {
+    palat = [];
+  });
+
+  it('Kuvio on oikean muotoinen', function() {
+    eval(fileContents('piirra.js'));
+    lueKomennot();
+    expect(palat).to.deep.equal(['y', 'y', 'o', 'o', 'a', 'a', 'o', 'o', 'y', 'y', 'o', 'o', 'a', 'a', 'o', 'o', 'y', 'y', 'o', 'o', 'a', 'a'], 'Varmista, että piirtämäsi kuva näyttää täsmälleen oikealta.');
+  });
+});
