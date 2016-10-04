448c2f75bf266dd84be95f4dc823bdf590561262
Henrik Nygren <nygrenh@gmail.com>
2016-10-04 11:39:51 +0300

    Add tests for listan summaus

--- toisto/listan_summaus/test/pointless_test.js
+++ toisto/listan_summaus/test/pointless_test.js
@@ -1,1 +1,19 @@
-console.log('This test is pointless.')
+context('Staattiset testit', function() {
+    it('Koodi kääntyy', function() {
+        expect(importingFile('listat.js')).not.to.throw(Error, 'Kirjoitit jotakin, mitä tietokone ei ymmärtänyt.');
+    });
+});
+
+context('Dynaamiset testit', function() {
+    var console;
+
+    beforeEach(function() {
+        console = {};
+        console.log = sinon.spy();
+    });
+
+    it('Tulostus on oikea', function() {
+        eval(fileContents('listat.js'));
+        expect(console.log.lastCall.args[0]).to.equal('Listan summa: 1337', 'Listan summa ei ollut oikea.');
+    });
+});
